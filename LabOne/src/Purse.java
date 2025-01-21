34b208da8ba915b7087b8a79fde55c6d3cea76ee
Bminister <bminister@cub.uca.edu>
2025-01-21 14:38:23 -0600

    Update to this, not getting too far

--- LabOne/src/Purse.java
+++ LabOne/src/Purse.java
@@ -1,25 +1,54 @@
 import java.util.Map;
+import java.util.HashMap;
+
 
 public class Purse {
-  Map<Denomination,Integer> cash; //represents the money in the purse
-    public void add(Denomination d, int amount) {
-        //Adds a number of a particular dem=nomination
-        cash.put(d, amount);
-    }
-    public double remove(Denomination d, int amount) {
-        //removes the money in the purse and returns that amount
-        double removed = d.amt * amount;
-        cash.remove(d, amount);
-        return removed;
-    }
-    public double getValue() //returns the amount of money in the purse
-    {
-        return 0.0;
-    }
-    public String toString()//returns a string representation of the purse and its contents
-    {
-        return "money";
+    public Purse() {
+        //Initialize all cash values in a purse to 0
+        final Denomination[] US_CURRENCIES = {
+                new Denomination("Hundred-Dollar Note",100.0,"Bill","images/hundred.png"),//Change with the actual .png name
+                new Denomination("Fifty-Dollar Note", 50.0,"Bill","images/fifty-dollar.png"),
+                new Denomination("Twenty-Dollar Note", 20.0,"Bill","images/twenty-dollar.png"),
+                new Denomination("Ten-Dollar Note", 10.0,"Bill","images/ten-dollar.png"),
+                new Denomination("Five-Dollar Note", 5.0,"Bill","images/five-dollar.png"),
+                new Denomination("One-Dollar Note", 10.0,"Bill","images/one-dollar.png"),
+                new Denomination("Quarter", 0.25,"Coin","images/quarter.png"),
+                new Denomination("Dime", 0.10, "Coin","images/dime.png"),
+                new Denomination("Nickel", 0.05, "Coin","images/nickel.png"),
+                new Denomination("Penny", 0.01, "Coin","images/penny.png"),
+        };
+        HashMap<Denomination, Integer> cash = new HashMap<>();
+        for (Denomination d : US_CURRENCIES) {
+            cash.put(d, 0);
+        }
     }
 
+    /*
+    public void cash(){
+        //Initialize all cash values in a purse to 0
+        final Denomination[] US_CURRENCIES = {
+                new Denomination("Hundred-Dollar Note",100.0,"Bill","images/hundred.png"),//Change with the actual .png name
+                new Denomination("Fifty-Dollar Note", 50.0,"Bill","images/fifty-dollar.png"),
+                new Denomination("Twenty-Dollar Note", 20.0,"Bill","images/twenty-dollar.png"),
+                new Denomination("Ten-Dollar Note", 10.0,"Bill","images/ten-dollar.png"),
+                new Denomination("Five-Dollar Note", 5.0,"Bill","images/five-dollar.png"),
+                new Denomination("One-Dollar Note", 10.0,"Bill","images/one-dollar.png"),
+                new Denomination("Quarter", 0.25,"Coin","images/quarter.png"),
+                new Denomination("Dime", 0.10, "Coin","images/dime.png"),
+                new Denomination("Nickel", 0.05, "Coin","images/nickel.png"),
+                new Denomination("Penny", 0.01, "Coin","images/penny.png"),
+        };
+        HashMap<Denomination, Integer> cash = new HashMap<>();
+        for (Denomination d : US_CURRENCIES) {
+            cash.put(d, 0);
+        }
+    }
+
+     */
+
+    //add(type: Denomination, num: int): void  // adds a number of a particular denomination
+    public void add(Denomination d, int amount)
+    {
+
+    }
 }
-
--- LabOne/src/Register.java
+++ LabOne/src/Register.java
@@ -1,11 +1,25 @@
 public class Register {
     public Purse makeChange(double amt)
     {
-        Purse p = new Purse();
+        //takes an amt and returns a Purse containing that amount in the fewest number of bills and coins
+        Purse p  = new Purse();
+        double temp = amt;
+        int NoteAmt;
+        if (temp / 100 > 0)
+        {
+            NoteAmt = (int)(temp / 100);
+            p.add(new Denomination("Hundred-Dollar Note",100.0,"Bill","images/hundred.png"),NoteAmt);
+            temp -= temp % 100;
+        }
         return p;
     }
     public static void main(String[] args) {
         //creates a register and demonstrates that it works
+
+        Register r = new Register();
+        Purse p = r.makeChange(300);
+        System.out.println(p);
+
     }
 
 }
