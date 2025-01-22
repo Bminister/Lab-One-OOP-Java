5a171a28ed9fcccd563f249bb5ed1e5cea23f0bc
Bminister <bminister@cub.uca.edu>
2025-01-22 15:50:23 -0600

    Works on GUI not done at all but done for the day

--- LabOne/src/MakingChange.java
+++ LabOne/src/MakingChange.java
@@ -10,5 +10,9 @@ public class MakingChange {
     public static void main(String[] args) {
         JFrame frame = new JFrame("Making Change");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 500);
+        frame.setVisible(true);
+        frame.add(new RegisterPanel());
+
     }
 }
--- LabOne/src/RegisterPanel.java
+++ LabOne/src/RegisterPanel.java
@@ -7,9 +7,19 @@ import java.awt.event.ActionListener;
 public class RegisterPanel extends JPanel {
     private Register register; // a register object to run the logic
     private JPanel inputPanel; //a panel to hold the JTextField for the user to enter an amount
-    private JTextField  input; // a place for the user to enter the amount
+    private JTextField input; // a place for the user to enter the amount
     private PursePanel changePanel; // a panel to display the change
+RegisterPanel() {
+    register = new Register();
+    this.setPreferredSize(new Dimension(100, 100));
 
+    JPanel inputPanel = new JPanel();
+    input = new JTextField();
+    inputPanel.add(new JLabel("Enter Amount: "));
+    inputPanel.add(input);
+
+
+}
 
 
 
